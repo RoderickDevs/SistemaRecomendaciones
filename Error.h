05aa0f0bb92d2f99a734b329fcff05c455239d1b
Jesus E. Dominguez <ametemoc@gmail.com>
2020-11-27 13:50:23 -0600

    Errores.h update

---
+++ Error.h
@@ -0,0 +1,79 @@
+//
+//  Error.h
+//  
+//
+//  Creado por Rodrigo Garcia Diaz y Jesus Enrique Domìnguez el 21 de Noviembre del 2020.
+//
+
+#ifndef Error_h
+#define Error_h
+
+
+/*
+* System headers required by the following declarations
+* (the implementation will import its specific dependencies):
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* Application specific headers required by the following declarations
+* (the implementation will import its specific dependencies):
+*/
+
+/* Constants declarations. */
+
+/* Set EXTERN macro: */
+
+#ifndef Error_IMPORT
+    #define EXTERN
+#else
+    #define EXTERN extern
+#endif
+
+/* Types declarations. */
+
+
+/* Global variables declarations. */
+
+
+/* Function prototypes. */
+
+
+/*
+ *
+ * La funcion controlador_errores inserta el nuevo error a un archvio 
+ * que contiene los errores anteriores.
+ *
+ * @params
+ *      error (float):
+         Es el valor del error actual para insertar en el archivo
+        
+ * @returns
+        none
+*/
+
+
+EXTERN void controlador_errores(float error)
+
+
+/*
+ *
+ * La funcion controlador_errorpromedio calcula el error promedio 
+ * del archivo error.r
+ *
+        
+ * @returns
+        float
+*/
+
+
+EXTERN float controlador_errorpromedio();
+
+
+#undef Error_IMPORT
+#undef EXTERN
+
+
+#endif /* Error_h */
